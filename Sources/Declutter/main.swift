119892628fed63ad2ca1d2f256eee9f07000d495
Alexey Blinov <alexey@songkick.com>
2020-05-21 13:38:44 +0100

    Bump version to 0.1.9

--- Sources/Declutter/main.swift
+++ Sources/Declutter/main.swift
@@ -6,31 +6,31 @@ import Chalk
 
 struct Declutter: ParsableCommand {
 
-    private let version = "0.1.8"
-    
+    private let version = "0.1.9"
+
     @Argument(default: ".", help: "Folder to declutter")
     var path: String
 
     @Option(help: "Folders to ignore")
     var ignore: [String]
-    
+
     @Flag(name: .customLong("version"), help: .hidden)
     var printVersion: Bool
-    
+
     func run() throws {
         if printVersion {
             print(version)
             return
         }
-        
+
         var sourceFolder = try Folder(path: path)
         let standardizedPath = sourceFolder.path.standardizingPath
         sourceFolder = try Folder(path: standardizedPath)
-        
+
         let foldersToIgnore = try ignore.map { try Folder(path: $0) }
-        
+
         let result = try findDuplicateFiles(in: sourceFolder, ignoring: foldersToIgnore)
-        
+
         print(result)
     }
 }
